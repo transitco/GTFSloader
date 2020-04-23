19427d618f5498813c0f79cb14638ffd8ea3330f
cptcloud <sven.diebold@gmail.com>
2020-04-22 21:29:21 -0400

    Add API layer
    Change-Id: Ib374df8e4de8d941506f151beccfdf4552b8ca70

--- index.js
+++ index.js
@@ -1,20 +1,48 @@
-var mongoose = require('mongoose');
-var gtfs = require('gtfs');
+const mongoose = require('mongoose');
+const express = require('express');
+const gtfs = require('gtfs');
 const config = require('./gtfs/config.json');
-//const resolvers = require('./graphql/resolvers/resolvers.js')
+const zipper = require('zip-local');
 
 mongoose.connect('mongodb://mongo:27017/gtfs', {useNewUrlParser: true, useUnifiedTopology: true});
 
-var db = mongoose.connection;
+const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function() {
   // we're connected!
 });
 
-gtfs.import(config)
-.then(() => {
-  console.log('Import Successful');
-})
-.catch(err => {
-  console.error(err);
+const app = express();
+
+app.post('/import_gtfs', (req, res, next) => {
+  gtfs.import(config)
+      .then(() => {
+        console.log('Import Successful');
+        res.send({ message: 'Import Successful' });
+        return mongoose.connection.close();
+      })
+      .catch((err) => {
+        console.error(err);
+        res.send({ message: 'Import failed' });
+      });
 });
+
+app.get('/export_gtfs', (req, res, next) => {
+  gtfs.export(config)
+      .then(() => {
+        console.log('Export Successful');
+        zipper.sync.zip('/usr/src/app/gtfsloader/gtfs-export/rtl_gtfs').compress().save('rtl_gtfs.zip');
+        const file = '/usr/src/app/gtfsloader/gtfs-export/rtl_gtfs/rtl_gtfs.zip';
+        res.download(file);
+        return mongoose.connection.close();
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+});
+
+app.listen(3000, () => {
+  console.log('Server running on port 3000');
+});
+
+
